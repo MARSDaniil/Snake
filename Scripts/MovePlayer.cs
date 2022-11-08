0deb3f5d59be79bec8eecb6e658786c633719ace
MARS <61511540+MARSMPEI@users.noreply.github.com>
2022-11-08 23:57:38 +0300

    add food, food moving, box collider to food

--- Scripts/MovePlayer.cs
+++ Scripts/MovePlayer.cs
@@ -5,14 +5,48 @@ using UnityEngine;
 public class MovePlayer : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector2 inputController;
+    private string inputKey;
+    public float speed;
 
+   
+    
     // Update is called once per frame
     void Update()
     {
+        CheckInput();
+
+    }
+
+
+  
+    private void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            inputController = new Vector2(0, 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            inputController = new Vector2(0, -1);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            inputController = new Vector2(1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            inputController = new Vector2(-1, 0);
+        }
+    }
+    private void FixedUpdate()
+        {
+        CheckInput();
+        this.transform.position = new Vector2(
+           Mathf.Round(this.transform.position.x) + inputController.x,
+           Mathf.Round(this.transform.position.y) + inputController.y
+           
+            ) ;
         
     }
 }
